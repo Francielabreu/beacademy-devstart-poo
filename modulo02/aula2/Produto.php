96600d918bbc232eeab5e12c8551156ebbb3ee79
Franciel Abreu <francielabreu23@gmail.com>
2022-06-06 23:15:02 -0300

    trabalhando com encapsulamento

--- modulo02/aula2/Produto.php
+++ modulo02/aula2/Produto.php
@@ -3,7 +3,29 @@
 declare(strict_types=1);
 
 class Produto{
+    //atributos
 
     private string $nome;
     private float $valor;
+
+    //metodos
+public function pegarNome():string
+{
+    return $this->nome;
 }
+
+public function alterarNome(string $novoNome):void
+{
+    $this->nome = $novoNome;
+}
+
+public function alterarValor(float $novoValor):void
+{
+    if ($novoValor < 0) {
+        die('Ops, valor não pode ser negativo!');
+    }
+    $this->valor = $novoValor;
+}
+
+}
+
--- modulo02/aula2/index.php
+++ modulo02/aula2/index.php
@@ -2,6 +2,7 @@
 include 'Produto.php';
 
 
+/*
 $produto1 = new Produto();
 $produto1->nome = 'Tenis para corrida';
 $produto1->valor = 299;
@@ -11,7 +12,11 @@ $produto2 = new Produto();
 $produto2->nome = 'Mochila';
 $produto2->valor = 100;
 
-var_dump($produto1);
-var_dump($produto2);
+*/
 
+$produto3 = new Produto();
+$produto3->alterarNome('Raquete de mata mosquito');
+$produto3->alterarValor(20);
+
+var_dump($produto3);
 
