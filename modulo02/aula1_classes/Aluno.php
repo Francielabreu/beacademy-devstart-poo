56b29378367f2050a6b35170b9f7a0449435eb7f
Franciel Abreu <francielabreu23@gmail.com>
2022-06-06 22:04:13 -0300

    Criação das classes e dos atributos

---
+++ modulo02/aula1_classes/Aluno.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+class Aluno{
+
+    public $nome;
+    public $cpf;
+
+   
+}
---
+++ modulo02/aula1_classes/Curso.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+class Curso{
+    
+    public $nome;
+    public $descricao;
+    public $cargaHoraria;
+
+
+}
