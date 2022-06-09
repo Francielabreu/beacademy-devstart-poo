8837152eba90c907d6d3a097692ebb9ce43064e1
Franciel Abreu <francielabreu23@gmail.com>
2022-06-08 23:58:39 -0300

    implementação das rotas e da chamada das views

--- modulo02/projeto-final/public/index.php
+++ modulo02/projeto-final/public/index.php
@@ -4,18 +4,45 @@ ini_set('display_errors',1);
 include '../vendor/autoload.php';
 
 use App\Controller\CategoryController;
+use App\Controller\ErrorController;
 use App\Controller\IndexController;
 use  App\Controller\ProductController;
 
-$c = new IndexController();
-$c->loginAction();
 
-$p = new ProductController();
-$p->listAction();
-$p->addAction();
-$p->editAction();
+$url = explode('?' , $_SERVER['REQUEST_URI'])[0];
 
-$c = new CategoryController();
-$c->listCategory();
-$c->addCategory();
-$c->editCategory();
+function createRoute(string $controllerName, string $methodName){
+    return [
+        'controller'=> $controllerName,
+        'method' => $methodName,
+
+    ];
+
+}
+
+$routes = [
+    '/'=> createRoute(IndexController::class, 'indexAction'),
+    '/produtos' => createRoute(ProductController::class, 'listAction'),
+    '/produtos/novo' => createRoute(ProductController::class, 'addAction'),
+
+
+];
+
+if (false ===isset($routes[$url])){
+    (new ErrorController())->notFoundAction();
+    exit;
+}
+
+$controllerName = $routes[$url] ['controller'];
+$methodName = $routes[$url] ['method'];
+
+(new $controllerName())-> $methodName();
+
+
+
+
+
+
+
+
+
