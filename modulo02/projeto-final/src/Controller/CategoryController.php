9042fd8a735f07d9b83c2e60506640ef287a23de
Franciel Abreu <francielabreu23@gmail.com>
2022-06-08 23:02:51 -0300

    criação das view e implementação do controller de categorias

--- modulo02/projeto-final/src/Controller/CategoryController.php
+++ modulo02/projeto-final/src/Controller/CategoryController.php
@@ -6,6 +6,18 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-class CategoryController{
+class CategoryController extends AbstractController{
 
+    public function listCategory():void
+    {
+        parent::render('Category/Listar');
+    }
+    public function addCategory():void
+    {
+       parent::render('Category/Adicionar');
+    }
+    public function editCategory():void
+    {
+       parent::render('Category/Editar');
+    }
 }
